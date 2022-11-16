0bcf1d7ce9fddd143797da9119cd2f3159722b1d
Bartek Tusiński <b.tusinski27@gmail.com>
2022-11-16 13:10:36 +0100

    Zadanie Chytrus poprawione

--- Chytrus/Chytrus/Chytrus.cpp
+++ Chytrus/Chytrus/Chytrus.cpp
@@ -1,48 +1,47 @@
-﻿//Zadanie: Chytrus. Generalnie to to samo co zadanie z ruletką tylko na odwrót. Komentarze sobie skasuj jak będziesz wysyłał rozwiązanie.
-
-#include <iostream>
+﻿#include <iostream>
+#include <vector>
 
 using namespace std;
 
 int main()
 {
-    const int MAX = 10000; 
+    const int MAX = 1000000; 
     int wszystkie_cukierki;
 
-    //Podajemy liczbę losowań, czyli elementów tablicy
     cin >> wszystkie_cukierki;
-    int tab[MAX];
+    int * tab = new int[MAX];
 
-    int min_czestosc = MAX; //Odwrotnie jak w zadaniu z ruletką, minimalna częstość musi startowo być ustawiona na max wartosc
+    int min_czestosc = MAX;
     int najrzadziej_wystepujacy_element;
 
     for (int i = 0; i < wszystkie_cukierki; i++)
     {
-        cin >> tab[i]; //wpisywanie wartości do tablicy
+        cin >> tab[i];
+    }
+
+    for (int i = 0; i < wszystkie_cukierki; i++)
+    {
 
         int czestosc = 0;
 
         for (int j = 0; j < wszystkie_cukierki; j++)
         {
-            if (tab[j] == tab[i])
+
+
+            if (tab[i] == tab[j])
             {
                 czestosc++;
             }
         }
 
-        //W tym warunku jest główna różnica pomiędzy tym zadaniem a tym z ruletką
-        //Warunek jest odwrotnie niż w poprzednim zadaniu - tu szukamy minimalnej częstości występowania
         if (czestosc < min_czestosc)
         {
             min_czestosc = czestosc;
             najrzadziej_wystepujacy_element = tab[i];
         }
 
-        //Ten warunek jest wykonywany jeśli częstość występowania jest taka sama, jeżeli tak jest wybieramy liczbę o większej wartości
-        else if (czestosc == min_czestosc)
+        if (czestosc == min_czestosc)
         {
-
-
             if (najrzadziej_wystepujacy_element > tab[i])
             {
                 najrzadziej_wystepujacy_element = najrzadziej_wystepujacy_element;
